2d0dd4045a65496026aacc51ff31a836ef1d5389
Rahul9851500 <rahul.kumar@msu.edu.in>
2025-01-19 00:03:27 +0530

    img

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,48 @@
 import React, { useState } from 'react';
-import './styles/Header.css';
+import './styles/Header.css';  // Ensure the correct path to your CSS file
 import { Close } from '@material-ui/icons'; // Ensure Material-UI icons are installed
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import { MenuBookOutlined } from '@mui/icons-material';
 
-
 const Header = () => {
+    const [active, setActive] = useState(false);
 
-    const [actve, setActve] =useState(false);
+    const showMenu = () => {
+        setActive(!active);
+    };
 
-    const showMenu =() => {
-        setActve(!actve)
-    }
-
-  return (
-    <div className="header">
-      <div className="header_logo">
-        <h1>Tabahi</h1>
-      </div>
-      <nav>
-        <ul>
-          <li className="closed">
-            <Close className="close" onclick={showMenu}/>
-          </li>
-          <li>
-            <Link to="/"><b>Home</b></Link> 
-          </li>
-          <li>
-            <Link to="/"><b>About</b></Link> 
-          </li>
-          <li>
-            <Link to="/"><b>Portfolio</b></Link> 
-          </li>
-          <li>
-            <Link to="/"><b>Blog</b></Link> 
-          </li>
-          <li>
-            <Link to="/"><b>Contact</b></Link> 
-          </li>
-        </ul>
-      </nav>
-      <div className='changer'>
-            <MenuBookOutlined className='menu' onclick ={showMenu}/>
-      </div>
-    </div>
-  );
+    return (
+        <div className="header">
+            <div className="header_logo">
+                <h1>Tabahi</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li className="closed">
+                        <Close className="close" onClick={showMenu} />
+                    </li>
+                    <li>
+                        <Link to="/"><b>Home</b></Link>
+                    </li>
+                    <li>
+                        <Link to="/"><b>About</b></Link>
+                    </li>
+                    <li>
+                        <Link to="/"><b>Portfolio</b></Link>
+                    </li>
+                    <li>
+                        <Link to="/"><b>Blog</b></Link>
+                    </li>
+                    <li>
+                        <Link to="/"><b>Contact</b></Link>
+                    </li>
+                </ul>
+            </nav>
+            <div className="changer">
+                <MenuBookOutlined className="menu" onClick={showMenu} />
+            </div>
+        </div>
+    );
 };
 
 export default Header;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,35 @@
 import React from "react";
-import './styles/Main.css';
+import './styles/Main.css'; 
+import { Facebook, Instagram, Twitter, LinkedIn } from '@mui/icons-material';
+import './React_img/background.jpg';
+const Main = () => {
+    return (
+        <div className="main">
+            <div className="main_container">
+                <div className="main_content">
+                    <div className="text">
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <p>Hello Everyone!</p>
+                        <h1>
+                            I am Rahul
+                        </h1>
+                        <p>BCA Student - 202204100028</p>
 
-const Main =() => {
-    return(
-        <div className="main_container">
-            <div className="Main_content">
-                <div className="text">
-                    <p>Hello Everyone !</p>
-                    <h1>
-                        I am Rahul
-                    </h1>
-                    <p>BCA Student-202204100028</p>
+                        <div className="icons">
+                            <Twitter className="icon" />
+                            <Instagram className="icon" />
+                            <Facebook className="icon" />
+                            <LinkedIn className="icon" />
+                        </div>
+                    </div>
                 </div>
             </div>
-
         </div>
-    )
+    );
 }
-export default Main
+
+export default Main;
